1023d0a421a1763abb99ffe3fa7b571411957458
rodrigo.arantes <rodrigo.arantes@totvs.com.br>
2019-05-21 16:00:20 -0300

    Alterado o Tomcat para Jetty, e trocado porta e contexto raiz da aplicacao

--- src/main/kotlin/br/com/in6/kotlin/KotlinApiApplication.kt
+++ src/main/kotlin/br/com/in6/kotlin/KotlinApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinApiApplication
-
-fun main(args: Array<String>) {
-	runApplication<KotlinApiApplication>(*args)
+class KotlinApiApplication {
+	fun main(args: Array<String>) {
+		runApplication<KotlinApiApplication>(*args)
+	}
 }
